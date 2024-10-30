d40c6603d80407204601881b074c9e2301dd6129
Henri Remonen <henri@remonen.fi>
2024-10-30 20:09:36 +0200

    feat: test app with simple web page

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/HRemonen/Grawlr/internal/crawler"
+	"github.com/HRemonen/Grawlr/internal/fetcher"
+)
+
+func main() {
+	client := &http.Client{}
+	fetcher := fetcher.NewHttpFetcher(client)
+	crawler := crawler.NewHttpCrawler(fetcher)
+	crawler.Crawl("https://web-scraping.dev/", 2)
+
+	// Output:
+	
+}
