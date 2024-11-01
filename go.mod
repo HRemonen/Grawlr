b81588b4f59b61b81b48c6ded1060e8af6f79cd4
Henri Remonen <henri@remonen.fi>
2024-11-01 13:31:28 +0200

    feat: implement robots.txt compliance -refs: #2

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/HRemonen/Grawlr
 
 go 1.23.1
 
-require golang.org/x/net v0.30.0
+require (
+	github.com/temoto/robotstxt v1.1.2
+	golang.org/x/net v0.30.0
+)
--- internal/fetcher/fetcher.go
+++ internal/fetcher/fetcher.go
@@ -19,12 +19,20 @@ package fetcher
 import (
 	"bytes"
 	"context"
+	"errors"
 	"io"
 	"log"
 	"net/http"
+	"net/url"
+	"sync"
 
 	"github.com/HRemonen/Grawlr/internal/web"
+
+	"github.com/temoto/robotstxt"
 )
+
+// ErrRobotsDisallowed is returned when a URL is disallowed by robots.txt.
+var ErrRobotsDisallowed = errors.New("URL is disallowed by robots.txt")
 
 // Fetcher is an interface that defines the behavior of a web page fetcher.
 type Fetcher interface {
@@ -33,19 +41,29 @@ type Fetcher interface {
 
 // HTTPFetcher is a Fetcher that uses an http.Client to fetch web pages.
 type HTTPFetcher struct {
-	Client *http.Client
+	Client       *http.Client
+	ignoreRobots bool
+	robotsMap    map[string]*robotstxt.RobotsData
+	lock         *sync.Mutex
 }
 
 // NewHTTPFetcher creates a new HTTPFetcher with the given http.Client.
 func NewHTTPFetcher(client *http.Client) *HTTPFetcher {
 	return &HTTPFetcher{
-		Client: client,
+		Client:       client,
+		ignoreRobots: false,
+		robotsMap:    make(map[string]*robotstxt.RobotsData),
+		lock:         &sync.Mutex{},
 	}
 }
 
 // Fetch fetches the web page at the given URL and return a custom Response object.
 func (f *HTTPFetcher) Fetch(url string) (web.Response, error) {
 	ctx := context.Background()
+
+	if err := f.checkRobots(url); err != nil {
+		return web.Response{}, err
+	}
 
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, http.NoBody)
 	if err != nil {
@@ -77,4 +95,44 @@ func (f *HTTPFetcher) Fetch(url string) (web.Response, error) {
 		Request:    req,
 		Headers:    &resp.Header,
 	}, nil
+}
+
+func (f *HTTPFetcher) checkRobots(u string) error {
+	if f.ignoreRobots {
+		return nil
+	}
+
+	parsedURL, err := url.Parse(u)
+	if err != nil {
+		return err
+	}
+
+	f.lock.Lock()
+	robot, ok := f.robotsMap[parsedURL.Host]
+	f.lock.Unlock()
+
+	if !ok {
+		robotURL := parsedURL.Scheme + "://" + parsedURL.Host + "/robots.txt"
+		resp, err := f.Client.Get(robotURL)
+		if err != nil {
+			return err
+		}
+
+		defer resp.Body.Close()
+
+		robot, err = robotstxt.FromResponse(resp)
+		if err != nil {
+			return err
+		}
+
+		f.lock.Lock()
+		f.robotsMap[parsedURL.Host] = robot
+		f.lock.Unlock()
+	}
+
+	if !robot.TestAgent(parsedURL.Path, "Grawlr") {
+		return ErrRobotsDisallowed
+	}
+
+	return nil
 }
