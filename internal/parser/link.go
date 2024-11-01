f96a36de86ada8a8bc287852b166ea1b6e626b09
Henri Remonen <henri@remonen.fi>
2024-11-01 12:11:38 +0200

    feat: handle relative links

--- internal/parser/link.go
+++ internal/parser/link.go
@@ -1,7 +1,9 @@
 package parser
 
 import (
-	"strings"
+	"fmt"
+	"log"
+	"net/url"
 
 	"github.com/HRemonen/Grawlr/internal/web"
 	"golang.org/x/net/html"
@@ -21,6 +23,12 @@ func (p *LinkParser) Parse(content web.Response) ([]string, error) {
 	links := []string{}
 	tokenizer := html.NewTokenizer(content.Body)
 
+	// Ensure content.Request.URL is a valid URL
+	baseURL, err := url.Parse(content.Request.URL.String())
+	if err != nil {
+		return nil, fmt.Errorf("invalid base URL: %v", err)
+	}
+
 	for {
 		tt := tokenizer.Next()
 
@@ -30,8 +38,7 @@ func (p *LinkParser) Parse(content web.Response) ([]string, error) {
 		case tt == html.StartTagToken:
 			t := tokenizer.Token()
 
-			isAnchor := t.Data == "a"
-			if !isAnchor {
+			if t.Data != "a" {
 				continue
 			}
 
@@ -40,11 +47,15 @@ func (p *LinkParser) Parse(content web.Response) ([]string, error) {
 				continue
 			}
 
-			// Checking that the href is absolute
-			// TODO: Handle relative URLs
-			hasProto := strings.Index(href, "http") == 0
-			if hasProto {
-				links = append(links, href)
+			linkURL, err := baseURL.Parse(href)
+			if err != nil {
+				log.Println("Error parsing link:", href, err)
+				continue
+			}
+
+			// Check if the scheme is valid
+			if linkURL.Scheme == "http" || linkURL.Scheme == "https" {
+				links = append(links, linkURL.String())
 			}
 		}
 	}
