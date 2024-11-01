bf6ba553b52e55dedb35b7a64d8b159e3ad9709e
Henri Remonen <henri@remonen.fi>
2024-11-01 11:19:05 +0200

    feat: extract response type to internal web package

---
+++ internal/web/response.go
@@ -0,0 +1,16 @@
+package web
+
+import (
+	"context"
+	"io"
+	"net/http"
+)
+
+// Response is a representation of the response from a Fetcher.
+type Response struct {
+	StatusCode int
+	Body       io.Reader
+	Ctx        *context.Context
+	Request    *http.Request
+	Headers    *http.Header
+}
